d08900809952898f53a5abf1fdee13a5feed1e78
chengyq <chengyq@chengyqdeiMac.local>
2016-10-25 09:53:04 +0800

    init project

---
+++ IMPlus/src/main/java/com/ctrip/implus/explore/MainActor.java
@@ -0,0 +1,12 @@
+package com.ctrip.implus.explore;
+
+/**
+ * Created by chengyq on 10/25/16.
+ */
+public class MainActor {
+
+    public static void main(String[] args) {
+
+        System.out.println("main actor");
+    }
+}
